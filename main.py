f1c98d5ffc3ccf5334c17050434aa6790badf6d1
Ronald Romario Gutierrez Arratia <113565299+Ronald-Gutierrez@users.noreply.github.com>
2022-10-05 19:51:47 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL,MySQLdb
-
+import json
 #https://www.it-swarm-es.com/es/python/usando-mysql-en-flask/941923326/
 # Para ejecutar el servicio debo ejecutar main.py
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '<contraseña>'
+app.config['MYSQL_PASSWORD'] = 'Iloveforyou'
 app.config['MYSQL_DB'] = 'Almacen'
 mysql = MySQL(app)
 
 app.secret_key='mysecretkey'
-
 
 @app.route('/')
 def home():
@@ -26,6 +25,12 @@ def main():
 def sign_in():
     return render_template('index.html')
 
+@app.route('/logout')
+def logout():
+ if 'correo' in session:
+  session.pop('correo', None)
+ return redirect(url_for('sign_in'))
+
 @app.route('/contact',methods=["GET","POST"])
 def contact():
     return render_template('contact.html')
@@ -34,30 +39,40 @@ def contact():
 def info():
     return render_template('info.html')
 
+@app.route('/uso',methods=["GET","POST"])
+def uso():
+    return render_template('uso.html')
 
-@app.route('/signUp', methods = ['POST','GET'])
-def signUp():
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
-    if  _email and _password:
-        conn = mysql.connect()
-        if (conn):
-            print("Conexion establecida")
-        else:
-            print("Conexion fallida")
-        cursor = conn.cursor()
-        cursor.callproc('crearUsuario',(_email, _password))
-        data = cursor.fetchall()
-        if len(data) ==0:
-            conn.commit()
-            print("Usuario fue creado!")
-            return json.dumps({'mensaje':'usuario fue creado!'})
-        else:
-            print({'error':str(data[0])})
-    else:
-        return json.dumps({'mensaje': 'Campos estan vacios!'})
+@app.route('/signUp', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        _email = request.form['correo']
+        _password = request.form['contra']
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO usuario (correo, password_) VALUES (%s,%s)", (_email, _password))
+        #cursor.callproc('crearUsuario',(_email,_password))
+        mysql.connection.commit()
+        flash('Nuevo contacto agregado')
+        return render_template('index.html')
     cursor.close()
-    conn.close()
+
+@app.route('/validateLogin', methods = ['POST'])
+def validateLogin():
+    _email = request.form['correo']
+    _password = request.form['contra']
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM usuario WHERE correo = %s AND password_ = %s", (_email, _password))  
+    data = cursor.fetchall()
+    if len(data) > 0:
+        session['user'] = _email
+        return redirect(url_for('transcripcion'))
+
+    return render_template('index.html', error = 'Usuario no existe')
+    cursor.close()
+
+@app.route('/transcripcion',methods = ['POST','GET'])
+def transcripcion():
+    return render_template('transcripcion.html')
 
 
 if __name__=='__main__':#si el archivo que se esta ejecutando es el main es decir el main.py entonces arranca el servidor
