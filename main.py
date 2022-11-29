6f5d5df9935f7a239d05445c8192bdc1c1f2e1c3
UbertoGC <84751424+UbertoGC@users.noreply.github.com>
2022-11-28 19:02:40 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from flask_mysqldb import MySQL, MySQLdb
+from flask_mysql_connector import MySQL
 from sonido import *
 
 #https://www.it-swarm-es.com/es/python/usando-mysql-en-flask/941923326/
@@ -11,15 +11,15 @@ app.debug = True
 app.secret_key ='secreto'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '209039'
-app.config['MYSQL_DB'] = 'almacen'
+app.config['MYSQL_PASSWORD'] = 'Hardware+10'
+app.config['MYSQL_DB'] = 'Almacen'
 mysql = MySQL(app)
 mysql.init_app(app)
 valor=["Presiona el boton de play para iniciar",0]
 app.secret_key='mysecretkey'
-dic_cantidad={'un':'1','uno':'1','dos':'2','tres':'3','cuatro':'4','cinco':'5','seis':'6','siete':'7','ocho':'8','nueve':'9','cero':'0'}
+dic_cantidad={'un':'1','una':'1','dos':'2','tres':'3','cuatro':'4','cinco':'5','seis':'6','siete':'7','ocho':'8','nueve':'9','cero':'0'}
 dic_u_medidas=['cucharada','pastilla']
-dic_tiempo=['hora','dia','semana','meses','mes','ano']
+dic_tiempo=['hora','día','semana','meses','mes']
 valor = []
 @app.route('/transcripcion',methods = ['POST','GET'])
 def transcripcion():
@@ -31,19 +31,20 @@ def transcripcion():
             menor=i.lower()
             diack=menor[:-1]
             resultados.append(diack.split())
+        print(resultados)
         for j in resultados:
             lista=["-","-","-","Indefinida"]
-            pri=True;
-            i = 0;
+            pri=True
+            i = 0
             while(i<len(j)):
                 if(pri):
                     while(i < len(j)):
                         if(j[i] in dic_cantidad):
                             if(lista[0] == "-"):
                                 lista[0] = ""
-                            lista[0] = lista[0]+dic_cantidad[j[k]]
+                            lista[0] = lista[0]+dic_cantidad[j[i]]
                         else:
-                            break;
+                            break
                         i+=1
                     if(i < len(j) and lista[0] != "-"):
                         if (j[i] in dic_u_medidas or (j[i])[:-1] in dic_u_medidas ):
@@ -53,18 +54,18 @@ def transcripcion():
                     pri = False
                     i-=1
                 elif(j[i] == "de"):
-                    k = i + 1;
-                    while(k<len(j)):
+                    k = i + 1
+                    while(k<len(j) and k>0):
                         if(j[k] != "cada" and j[k] != "por"):
-                            if(lista[2] == "-"):
-                                lista[2] = ""
-                            lista[2] = lista[2] + " " + j[k];
+                            if(lista[1] == "-"):
+                                lista[1] = ""
+                            lista[1] = lista[1] + " " + j[k]
                         else:
                             break
                         k += 1
                     i = (k - 1)
                 elif(j[i] == "cada" or j[i] == "por"):
-                    k=i+1;
+                    k=i+1
                     arc1="-"
                     arc2="-"
                     while(k<len(j)):
@@ -75,20 +76,21 @@ def transcripcion():
                         else:
                             break
                         k+=1
-                    det=4
-                    if(j[i]=="cada"):
-                        det=3
+                    det = 2
+                    if(j[i] == "por"):
+                        det = 3
+                    print(det)
                     if(k<len(j)):
                         if(j[k] in dic_tiempo or (j[k])[:-1] in dic_tiempo):
-                            arc2 = j[k];
+                            arc2 = j[k]
                             if(arc1!="-"):
                                 lista[det]= j[i] + " " + arc1 + " " + arc2
                             else:
                                 lista[det]= j[i] + " " + arc2
                         else:
                             lista[det]= "-"
-                    i=k;
-                i+=1;
+                    i=k
+                i+=1
             valor.append(lista)
     elif(estado[0] and (not loop.is_running())):
         guardado.clear()
@@ -145,6 +147,7 @@ def add_contact():
         _email = request.form['correo']
         _password = request.form['contra']
         cursor = mysql.connection.cursor()
+        cursor.execute("Use Almacen")
         cursor.execute("INSERT INTO usuario (correo, password_) VALUES (%s,%s)", (_email, _password))
         #cursor.callproc('crearUsuario',(_email,_password))
         mysql.connection.commit()
@@ -157,13 +160,15 @@ def validateLogin():
     _email = request.form['correo']
     _password = request.form['contra']
     cursor = mysql.connection.cursor()
+    cursor.execute("Use Almacen")
     cursor.execute("SELECT * FROM usuario WHERE correo = %s AND password_ = %s", (_email, _password))
     data = cursor.fetchall()
     if len(data) > 0:
         session['user'] = _email
         return redirect(url_for('transcripcion'))
     cursor.close()
-    return render_template('index.html', error = 'Usuario no existe')
+    flash('Contraseña o usuario incorrecto')
+    return redirect('sign_in')
 
 if __name__=='__main__':#si el archivo que se esta ejecutando es el main es decir el main.py entonces arranca el servidor
     app.run(port=5000,debug=True)#corre el servidor
